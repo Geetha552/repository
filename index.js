05c9d785fdac287be5bef46f51bfdde6884623a2
Geetha552 <vutukurigeethanl@gmail.com>
2025-11-21 14:56:28 +0530

    Update repository.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("This is my first repository")
+const profile = {
+  user: {
+    details: {
+      email: "test@mail.com"
+    }
+  }
+};
+
+
+console.log(profile.user.details.email);      
+console.log(profile.user.details?.phone);    
+
+
+const student = {
+  id: 101,
+  name: "Mia"
+ 
+};
+
+
+const streetAddress = student.location?.street;
+
+console.log(streetAddress);                 
